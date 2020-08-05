b0dfbe464836f6176725d23d7360da2e9013213f
Ryan Schie <rjschie@gmail.com>
2020-08-04 21:30:52 -0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  plugins: ['svelte3', 'cypress'],
+  extends: ['eslint:recommended'],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3',
+      env: {
+        browser: true,
+        es6: true,
+        node: true,
+      },
+    },
+    {
+      files: ['.eslintrc.js', '*.config.js', 'cypress/**/*', 'src/server.js'],
+      extends: ['plugin:cypress/recommended'],
+      env: {
+        node: true,
+      },
+    },
+  ],
+};
